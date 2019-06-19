b39811e0d317f4ee363a496541af7ceaf61cc7e0
saheel-ahmed <saheelahmed7@gmail.com>
2019-06-19 19:41:14 +0400

    Page content added

--- src/app/en-pages/en-pages.component.ts
+++ src/app/en-pages/en-pages.component.ts
@@ -10,16 +10,45 @@ import { CmsPageService } from './../services/cms-page.service';
 })
 export class EnPagesComponent implements OnInit {
 
+  pageUris : any;
   pageContent : string = "";
+  pageFooter : string = "";
+  cmsPages : any;
+  pages : any;
 
   constructor(private route: ActivatedRoute, private router: Router,
   private elRef: ElementRef, private renderer: Renderer2, 
   private cmsPageService :CmsPageService
   ) { 
-
+    this.pageUris = this.cmsPageService.cmsPageURI;
+    this.cmsPages = this.cmsPageService.cmsPageResponse;
+    this.pageFooter = this.cmsPages.en.footer;
   }
 
   ngOnInit() {
+    this.route.params.subscribe(params => { 
+      if(this.pageUris.includes(params['page'])){
+        //this.pagename = params['page'];
+        //find page content
+        for(let page of this.cmsPages.en.pages){
+            if(page.link === 'en/'+params['page']){
+               this.pageContent = page.content;
+               document.title = page.title;
+               var metaList = document.getElementsByTagName("META");
+               metaList[1].setAttribute("content",page.metaDescription);
+               break;
+            }else{
+              this.pageContent = "<h1>Page content not yet set!</h1>";
+               document.title = "Coming soon!";
+               var metaList = document.getElementsByTagName("META");
+               metaList[1].setAttribute("content","noarchive");
+            }             
+        }
+       
+      }else{
+        this.router.navigate(['page/en/404']);
+      }      
+    });
   }
 
 }
--- src/app/routerConfig.ts
+++ src/app/routerConfig.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { EnPagesComponentComponent } from './en-pages-component/en-pages-component.component';
 import { EnPageNotFoundComponent } from './en-page-not-found/en-page-not-found.component';
+import { EnPagesComponent } from './en-pages/en-pages.component';
 
 const appRoutes: Routes = [
   { path: 'en/:page', 
-    component: EnPagesComponentComponent 
+    component: EnPagesComponent 
   },
   {
     path: 'page/en/404',
